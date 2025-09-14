2e49ab03e0f3074ce53fa9ee81fc0d9c6cb640f1
Jazmine Singh <jzmn65@gmail.com>
2025-09-13 22:35:55 -0400

    Initial Commit

---
+++ Layouts/LayoutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutsApp.swift
+//  Layouts
+//
+//  Created by Jazmine Singh on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LayoutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
