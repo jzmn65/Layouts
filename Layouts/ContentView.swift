96678e550622eb80b956a28e23c3119a863bc6fb
Jazmine Singh <jzmn65@gmail.com>
2025-09-15 14:51:19 -0400

    Layouts Lesson

--- Layouts/ContentView.swift
+++ Layouts/ContentView.swift
@@ -8,14 +8,60 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "When the Genius Bar Needs Help, They Call You!"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack {
+                
+                   
+                
+                Spacer()
+                
+                Text(message)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .minimumScaleFactor(0.5)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.darkPink)
+                    .frame(height: 150)
+                    .frame(maxWidth: .infinity)
+    //                .border(.orange, width: 2)
+                    .padding()
+                    
+                Spacer()
+                
+                Divider()
+                    .background(.black)
+                    .padding()
+                    .frame(width: 150.0)
+                Rectangle()
+                    .fill(.indigo)
+                    .frame(width: geometry.size.width*2/3, height: 1)
+                
+                HStack {
+                    Button("Awesome"){
+                        message = "You are Awesome"
+                    }
+                    Spacer()
+                    
+                    Button("Great"){
+                        message = "You are Great"
+                        
+                    }
+                }
+                .buttonStyle(.borderedProminent)
+    //            .border(.purple, width: 5)
+                .padding()
+                
+                Rectangle()
+                    .frame(height: 0)
+                    .background(.indigo)
+                
+            }
         }
-        .padding()
+           
+        
     }
 }
 
